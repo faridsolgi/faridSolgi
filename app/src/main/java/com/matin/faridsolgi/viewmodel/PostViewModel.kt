e06a8cf63faea456ad3d17b83921b4cefc2aa2ac
MAT-AGH <irprgrmr@gmail.com>
2022-07-16 15:20:50 +0430

    fSol: PostViewModel.kt live data of posts

--- app/src/main/java/com/matin/faridsolgi/viewmodel/PostViewModel.kt
+++ app/src/main/java/com/matin/faridsolgi/viewmodel/PostViewModel.kt
@@ -1,9 +1,16 @@
 package com.matin.faridsolgi.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.matin.faridsolgi.Posts
+import com.matin.faridsolgi.repository.PostRepository
+import javax.inject.Inject
 
-class PostViewModel : ViewModel() {
+class PostViewModel
+    @Inject constructor( postRepository: PostRepository) : ViewModel() {
 
+        val PostsLiveData :LiveData<Posts> = postRepository.getPosts(1,30).asLiveData()
 
 
 }
