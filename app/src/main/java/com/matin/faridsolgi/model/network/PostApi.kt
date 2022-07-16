32dfa10ad715fcb150eb7fb8cc422f907d199f80
MAT-AGH <irprgrmr@gmail.com>
2022-07-16 15:19:22 +0430

    fSol: PostApi.kt kotlin coroutine add

--- app/src/main/java/com/matin/faridsolgi/model/network/PostApi.kt
+++ app/src/main/java/com/matin/faridsolgi/model/network/PostApi.kt
@@ -1,12 +1,14 @@
 package com.matin.faridsolgi.model.network
 
+import com.matin.faridsolgi.Posts
+import kotlinx.coroutines.flow.Flow
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface PostApi {
 
     @GET("api/feed/?page={page}&limit={limit}")
-    fun getPosts(@Path("page") page : Int ,
+   suspend fun getPosts(@Path("page") page : Int ,
     @Path("limit") limit :Int
-    )
+    ): Posts
 }
