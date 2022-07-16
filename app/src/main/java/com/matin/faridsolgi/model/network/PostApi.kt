c70216b52bfa85b9c181a28819def03897cba9e9
MAT-AGH <irprgrmr@gmail.com>
2022-07-16 16:29:47 +0430

    fSol: fix bug query in request

--- app/src/main/java/com/matin/faridsolgi/model/network/PostApi.kt
+++ app/src/main/java/com/matin/faridsolgi/model/network/PostApi.kt
@@ -4,11 +4,12 @@ import com.matin.faridsolgi.Posts
 import kotlinx.coroutines.flow.Flow
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface PostApi {
 
-    @GET("api/feed/?page={page}&limit={limit}")
-   suspend fun getPosts(@Path("page") page : Int ,
-    @Path("limit") limit :Int
+    @GET("api/feed")
+   suspend fun getPosts(@Query("page") page : Int,
+                        @Query("limit") limit :Int
     ): Posts
 }
