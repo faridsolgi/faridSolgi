0ccd99edd5c51a1f0c4bc5460e2fe034dc9e3d01
MAT-AGH <irprgrmr@gmail.com>
2022-07-16 16:35:06 +0430

    fSol: transaction to PostFragment.kt

--- app/src/main/java/com/matin/faridsolgi/view/MainActivity.kt
+++ app/src/main/java/com/matin/faridsolgi/view/MainActivity.kt
@@ -3,12 +3,17 @@ package com.matin.faridsolgi.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.matin.faridsolgi.R
+import com.matin.faridsolgi.view.fragments.PostFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.clMainActivityBaseFragments,
+        PostFragment(),PostFragment::class.simpleName).commit()
 
 
     }
