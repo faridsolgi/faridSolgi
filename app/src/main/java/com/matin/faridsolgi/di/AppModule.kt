27bcaf3374db59529df00790c75bfc3cf2154abf
MAT-AGH <irprgrmr@gmail.com>
2022-07-16 15:21:28 +0430

    fSol: provide retrofit in di, AppModule.kt

--- app/src/main/java/com/matin/faridsolgi/di/AppModule.kt
+++ app/src/main/java/com/matin/faridsolgi/di/AppModule.kt
@@ -1,10 +1,21 @@
 package com.matin.faridsolgi.di
 
+import com.matin.faridsolgi.model.network.PostApi
+import com.matin.faridsolgi.model.network.RetrofitService
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class AppModule {
+
+
+    @Provides
+    @Singleton
+    fun provideRetrofit() : RetrofitService{
+        return RetrofitService()
+    }
 }
